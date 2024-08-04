47bce96d9613832ea031c01ad4720ae10dddcfc1
yosefHesham <yousefmobdev@gmail.com>
2024-08-04 20:33:35 +0300

    Add Date Picker

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,40 +1,52 @@
-"use client";
-
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-const formSchema = z.object({
-  description: z.string().min(4, {
-    message: "Username must be at least 2 characters.",
-  }),
-  fromTime: z.date().refine(
-    (date) => {
-      const today = new Date();
-      today.setHours(0, 0, 0, 0); // Set to midnight to compare only the date part
-      return date >= today;
-    },
-    {
-      message: "Date must be today or later",
+const formSchema = z
+  .object({
+    description: z.string().min(4, {
+      message: "Description must be at least 4 characters.",
+    }),
+    fromTime: z.date().refine(
+      (date) => {
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+        return date >= today;
+      },
+      {
+        message: "From time must be today or later",
+      }
+    ),
+    toTime: z.date(),
+  })
+  .superRefine((data, ctx) => {
+    if (data.toTime <= data.fromTime) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: ["toTime"],
+        message: "ToTime must be after FromTime",
+      });
     }
-  ),
-});
+  });
 
 export function ProfileForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       description: "",
+      fromTime: new Date(),
+      toTime: new Date(),
     },
   });
 
@@ -45,20 +57,60 @@ export function ProfileForm() {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="description"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
+        <FormItem>
+          <FormLabel>Description</FormLabel>
+          <FormControl>
+            <Controller
+              name="description"
+              control={form.control}
+              render={({ field }) => (
+                <Input placeholder="Write blog about js closures" {...field} />
+              )}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
 
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+        <FormItem>
+          <FormLabel>From Time</FormLabel>
+          <FormControl>
+            <Controller
+              name="fromTime"
+              control={form.control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <DatePicker
+                  selected={value ? new Date(value) : null}
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  showTimeSelect
+                  dateFormat="Pp"
+                />
+              )}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+
+        <FormItem>
+          <FormLabel>To Time</FormLabel>
+          <FormControl>
+            <Controller
+              name="toTime"
+              control={form.control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <DatePicker
+                  selected={value ? new Date(value) : null}
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  showTimeSelect
+                  dateFormat="Pp"
+                />
+              )}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+
         <Button type="submit">Submit</Button>
       </form>
     </Form>
